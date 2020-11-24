a58eff9ab3e1270423bd4d10e9838b04a0445054
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-24 11:17:13 +0530

    Add files via upload

---
+++ DAY 2.py
@@ -0,0 +1,17 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b,c=10,20,30
+>>> print("The answers are: ",a/10,b*50,c+60)
+The answers are:  1.0 1000 90
+>>> main_str="FAITH"
+>>> change_str=list(main_str)
+>>> change_str[2]="G"
+>>> main_str=''.join(change_str)
+>>> print("The string FAITH after replacement is ",main_str)
+The string FAITH after replacement is  FAGTH
+>>> a,b=5,8.2
+>>> print("Int to float= " ,float(a))
+Int to float=  5.0
+>>> print("Float to int= ",int(b))
+Float to int=  8
+>>> 
