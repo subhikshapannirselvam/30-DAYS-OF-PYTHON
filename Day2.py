11fc69c2e6d2e48ecc97de9dd5356696432fb3c3
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-24 10:53:44 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,10 @@
+a,b,c=10,20,30
+print("The answers are: ",a/10,b*50,c+60)
+main_str="FAITH"
+change_str=list(main_str)
+change_str[2]="G"
+main_str=''.join(change_str)
+print("The string FAITH after replacement is ",main_str)
+a,b=5,8.2
+print("Int to float= " ,float(a))
+print("Float to int= ",int(b))
