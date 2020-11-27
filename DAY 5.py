c399cdc5c478112ce55bec33fc054170c2cd5adf
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-27 11:50:56 +0530

    Add files via upload

---
+++ DAY 5.py
@@ -0,0 +1,12 @@
+def math_function (a,b):
+    print("Addition of two numbers",a+b)
+    print("Subraction of two numbers",a-b)
+    print("Multiplication of two numbers",a*b)
+    print("Division of two numbers",a/b)
+math_function(20,10)
+def covid(patient_name,temp="98 deg"):
+    print("The patient name is",patient_name ,"and temperature is",temp)
+covid("Tom")
+covid("Ben")
+covid("Raj")
+covid("Vish","100 deg")
