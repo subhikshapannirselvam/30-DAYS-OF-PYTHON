b55d607c229f1e8ee2fc6712669598e0225ef5cc
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-28 19:10:42 +0530

    Add files via upload

---
+++ DAY 6-5.py
@@ -0,0 +1,17 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(1,11):
+	print("9*",i,"=",i*9)
+
+	
+9* 1 = 9
+9* 2 = 18
+9* 3 = 27
+9* 4 = 36
+9* 5 = 45
+9* 6 = 54
+9* 7 = 63
+9* 8 = 72
+9* 9 = 81
+9* 10 = 90
+>>> 
