27f784f81e37adfdaee4f7cebeb182c039f3e7f2
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-28 19:53:41 +0530

    Add files via upload
    #math.cos( 0.5 ):  0.8775825618903728
    #math.sin( 0.5 ):  0.479425538604203
    #math.tan( 0.5 ):  0.5463024898437905
    #math.acos( 0.5 ):  1.0471975511965979
    #math.asin( 0.5 ):  0.5235987755982989
    #math.atan( 0.5 ):  0.4636476090008061
    #math.atan2( 2 , 0.5 ):  1.3258176636680326
    #math.hypot( 0.5 , 2 ):  2.0615528128088303

---
+++ DAY 6-8.py
@@ -0,0 +1,19 @@
+import math
+x = 0.50
+# math.cos()
+print("math.cos(",x,"): ", math.cos(x));
+# math.sin()
+print("math.sin(",x,"): ", math.sin(x));
+# math.tan()
+print("math.tan(",x,"): ", math.tan(x));
+# math.acos()
+print("math.acos(",x,"): ", math.acos(x));
+# math.asin()
+print("math.asin(",x,"): ", math.asin(x));
+# math.atan()
+print("math.atan(",x,"): ", math.atan(x));
+y = 2
+# math.atan2(y,x) = atan(y/x)
+print("math.atan2(",y,",",x,"): ", math.atan2(y,x))
+# math.hypot(x,y)
+print("math.hypot(",x,",",y,"): ", math.hypot(x,y))
