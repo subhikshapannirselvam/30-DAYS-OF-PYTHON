b464d71f4ddb15f936271ce8292cdb8db3d3fac5
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-29 13:17:19 +0530

    Add files via upload

---
+++ DAY 7-3.py
@@ -0,0 +1,10 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> print(random.randint(1,100))
+97
+>>> print(random.randint(1,100))
+98
+>>> print(random.randint(1,100))
+15
+>>> 
