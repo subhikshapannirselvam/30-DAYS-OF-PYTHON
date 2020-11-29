c43cb9804a4085247de7683ec292a1e534f852e8
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-29 13:17:31 +0530

    Add files via upload

---
+++ DAY 7-4.py
@@ -0,0 +1,15 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import sys
+>>> for p in sys.path:
+	print(p)
+
+	
+
+C:\Users\ELCOT\AppData\Local\Programs\Python\Python38-32\Lib\idlelib
+C:\Users\ELCOT\AppData\Local\Programs\Python\Python38-32\python38.zip
+C:\Users\ELCOT\AppData\Local\Programs\Python\Python38-32\DLLs
+C:\Users\ELCOT\AppData\Local\Programs\Python\Python38-32\lib
+C:\Users\ELCOT\AppData\Local\Programs\Python\Python38-32
+C:\Users\ELCOT\AppData\Local\Programs\Python\Python38-32\lib\site-packages
+>>> 
