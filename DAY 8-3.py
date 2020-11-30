292eb602955b4cf8cc6a0505ec3c3285105a7c00
subhikshapannirselvam <68904212+subhikshapannirselvam@users.noreply.github.com>
2020-11-30 18:54:27 +0530

    Add files via upload

---
+++ DAY 8-3.py
@@ -0,0 +1,13 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> try:
+	x="what is name error?"
+	print(xx)
+except NameError:
+	print("NameError occured.'xx' is not defined. Please check your variable!")
+else:
+	print("oops!Something went wrong.")
+
+	
+NameError occured.'xx' is not defined. Please check your variable!
+>>> 
